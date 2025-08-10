77c8e0cd5131acc33cd76ca6cfae501d3d109696
HoanNam <m8mi0909@gmail.com>
2025-08-10 13:39:26 +0700

    Xây dựng UI phần UpperSection component #1

---
+++ src/layouts/components/Footer/components/UpperSection/index.js
@@ -0,0 +1,62 @@
+import { memo } from 'react';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+import Button from '~/components/Button';
+import styles from './UpperSection.module.scss';
+
+const cx = classNames.bind(styles);
+
+function UpperSection() {
+  return (
+    <div className={cx('wrapper')}>
+      <div className='container'>
+        {/* Upper section */}
+        <div className={cx('upper-section')}>
+          {/* Newsletter */}
+          <div className={cx('newsletter')}>
+            {/* Newsletter title */}
+            <p className={cx('newsletter-title')}>
+              Join our newsletter for £10 offs
+            </p>
+
+            {/* Newsletter desc */}
+            <p className={cx('newsletter-desc')}>
+              Register now to get latest updates on promotions & <br />
+              coupons.Don’t worry, we not spam!
+            </p>
+          </div>
+
+          {/* Register */}
+          <div className={cx('register')}>
+            {/* Register field */}
+            <div className={cx('register-field')}>
+              {/* Icon */}
+              <div className={cx('envelop-icon')}>
+                <FontAwesomeIcon icon={faEnvelope} />
+              </div>
+
+              {/* Input */}
+              <input
+                className={cx('register-input')}
+                placeholder='Enter your email address'
+              />
+
+              {/* Button */}
+              <Button className={cx('register-btn')}>SEND</Button>
+            </div>
+
+            {/* Terms */}
+            <p className={cx('terms')}>
+              <span>By subscribing you agree to our </span>
+              <a href='#'>Terms & Conditions and Privacy & Cookies Policy.</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default memo(UpperSection);
--- src/layouts/components/Footer/index.js
+++ src/layouts/components/Footer/index.js
@@ -1,5 +1,15 @@
+import classNames from 'classnames/bind';
+import styles from './Footer.module.scss';
+import UpperSection from './components/UpperSection';
+
+const cx = classNames.bind(styles);
+
 function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <div className={cx('wrapper')}>
+      <UpperSection />
+    </div>
+  );
 }
 
 export default Footer;
